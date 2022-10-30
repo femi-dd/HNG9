9242ac72ae53d675bbf3e9bb7501f34da4ec837b
Femi_DD 😎 <28959886+femi-dd@users.noreply.github.com>
2022-10-30 15:10:20 -0400

    Update profile.js

--- profile.js
+++ profile.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-app.use(cors({ origin: true, credentials: true, }));
+app.use(cors({'access-control-allow-origin': '*'}));
 
 app.get('/backend_track/stage_1/', (request, response) => {
     response.send({
