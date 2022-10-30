308b12f8ab5a1258c0c651fae42eb5261eae6b12
Femi_DD 😎 <28959886+femi-dd@users.noreply.github.com>
2022-10-30 14:52:44 -0400

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.set('')
+
 app.get('/backend_track/stage_1/', (request, response) => {
+    response.header('Access-Control-Allow-Origin', '*');
     response.send({
         slackUsername: 'femi_dd',
         backend: true,
